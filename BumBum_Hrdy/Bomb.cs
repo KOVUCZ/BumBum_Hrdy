6d07763e51ccde2e3a0b04834d4de9ac4e9408bf
KOVUCZ <Hrdy.Adam@stud.vassboskovice.cz>
2023-10-09 11:20:17 +0200

    změny v Bomb.cs

--- BumBum_Hrdy/Bomb.cs
+++ BumBum_Hrdy/Bomb.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Threading;
 
 namespace BumBum_Hrdy
 {
     class Bomb
     {
+        public int Countdown { get; private set; }
+        public Action Detonation;
+
+        private DispatcherTimer timer;
+        
+        public Bomb(int TimeToDetonate)
+        {
+            Countdown = TimeToDetonate;
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0,0,1);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Countdown--;
+            if(Countdown < 0)
+            {
+                Detonation?.Invoke();
+            }
+        }
 
     }
 }
